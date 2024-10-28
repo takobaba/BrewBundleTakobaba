570355ffa5527396d6b369e41969c5030feb5ec6
T K <t.kaya@outlook.com>
2024-10-28 11:53:42 +1100

    Update brew_cask_update.sh

--- brew_cask_update.sh
+++ brew_cask_update.sh
@@ -39,14 +39,13 @@ fi
 # echo; echo "${BLACK}See\thttps://github.com/Homebrew/homebrew-cask/issues/88440\n\thttps://stackoverflow.com/questions/63651114/update-homebrew-casks-versions${NC}"
 # echo; read -n 1 -s -r -p "Press any key to proceed or Ctrl+C to abort..."; echo
 
-
 #
 # check outdated
 #
 
 # { echo; echo "${BLACK}Check if cask ${RED}$CASKNAME${BLACK} requires updates${NC}"; } 2> /dev/null
 if [ "$(brew outdated --cask --greedy $CASKNAME)" = "" ]; then
-  { echo "${RED}Looks like cask ${BLACK}$CASKNAME${RED} either not exists, or not needed to update${NC}"; } 2> /dev/null
+  { echo "${RED}Looks like cask ${BLACK}$CASKNAME${RED} either not exists, or not needed to update${NC}"; } 2>/dev/null
   exit
 fi
 
@@ -58,10 +57,10 @@ CURRENT=$(brew outdated --cask --greedy --json=v2 $CASKNAME | jq -r '.casks[0].c
 INSTALLATIONFOLDER=$(brew info --json=v2 $CASKNAME | jq ".casks[].artifacts[].app[0] | select( . != null )" | tr -d \")
 REAL_INSTALLED=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" /Applications/$INSTALLATIONFOLDER/Contents/Info.plist)
 if [ "$INSTALLED" = "latest" ]; then
-  { echo "${RED}Looks like cask ${BLACK}$CASKNAME${RED} has ${BLUE}latest${RED} version, ignore such casks${NC}"; } 2> /dev/null
+  { echo "${RED}Looks like cask ${BLACK}$CASKNAME${RED} has ${BLUE}latest${RED} version, ignore such casks${NC}"; } 2>/dev/null
   exit
 elif [ "$INSTALLED" = "$CURRENT" ]; then
-  { echo "${RED}Looks like cask ${BLACK}$CASKNAME${RED} has ${BLUE}${REAL_INSTALLED}${RED} version, Update Brew${NC}"; } 2> /dev/null
+  { echo "${RED}Looks like cask ${BLACK}$CASKNAME${RED} has ${BLUE}${REAL_INSTALLED}${RED} version, Update Brew${NC}"; } 2>/dev/null
   # Dont exit
 fi
 echo "${RED}Looks like cask ${BLACK}$CASKNAME${RED} has ${BLUE}${REAL_INSTALLED}${RED} version, with installed ${BLUE}${INSTALLED}"
@@ -70,13 +69,22 @@ echo "${RED}Looks like cask ${BLACK}$CASKNAME${RED} has ${BLUE}${REAL_INSTALLED}
 # backup current cask
 #
 
-{ echo; echo "${BLACK}Check/create backup directory ${RED}$BACKUPDIR${NC}"; } 2> /dev/null
+{
+  echo
+  echo "${BLACK}Check/create backup directory ${RED}$BACKUPDIR${NC}"
+} 2>/dev/null
 [[ ! -e "$BACKUPDIR" ]] && mkdir "$BACKUPDIR"
 
-{ echo; echo "${BLACK}Creating backup directory ${RED}$BACKUPDIR/$DATE${NC}"; } 2> /dev/null
+{
+  echo
+  echo "${BLACK}Creating backup directory ${RED}$BACKUPDIR/$DATE${NC}"
+} 2>/dev/null
 mkdir -p "$BACKUPDIR/$DATE"
 
-{ echo; echo "${BLACK}Backing up current cask files${NC}"; } 2> /dev/null
+{
+  echo
+  echo "${BLACK}Backing up current cask files${NC}"
+} 2>/dev/null
 cp -R "$CASKDIR/.metadata" "$BACKUPDIR/$DATE"
 cp -R "$CASKDIR/$INSTALLED" "$BACKUPDIR/$DATE"
 
@@ -84,7 +92,10 @@ cp -R "$CASKDIR/$INSTALLED" "$BACKUPDIR/$DATE"
 # rename installed cask version to current
 #
 
-{ echo; echo "${BLACK}Rename ${BLUE}$INSTALLED${BLACK} with ${BLUE}$CURRENT${NC}"; } 2> /dev/null
+{
+  echo
+  echo "${BLACK}Rename ${BLUE}$INSTALLED${BLACK} with ${BLUE}$CURRENT${NC}"
+} 2>/dev/null
 mv "$CASKDIR/$INSTALLED" "$CASKDIR/$CURRENT"
 mv "$CASKDIR/.metadata/$INSTALLED" "$CASKDIR/.metadata/$CURRENT"
 
@@ -96,6 +107,8 @@ done
 # replace cask script
 #
 
-{ echo; echo "${BLACK}Replace cask script with latest version${NC}"; } 2> /dev/null
-brew cat --cask $CASKNAME > "$CASKDIR/.metadata/$CURRENT/$DATE/Casks/$CASKNAME.rb"
-
+{
+  echo
+  echo "${BLACK}Replace cask script with latest version${NC}"
+} 2>/dev/null
+brew cat --cask $CASKNAME >"$CASKDIR/.metadata/$CURRENT/$DATE/Casks/$CASKNAME.rb"
